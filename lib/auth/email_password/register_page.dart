8d10bbdfc2dc4cbff945d42c9e99cb213bafba6d
dariopmaciel <dariodepaulamaciel@hotmail.com>
2023-12-05 18:37:55 -0300

    ok Gravando no console

---
+++ lib/auth/email_password/register_page.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _emailnEC = TextEditingController();
+  final _senhaEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailnEC.dispose();
+    _senhaEC.dispose();
+    super.dispose();
+  }
+
+  Future<void> registerUser() async {
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailnEC.text, password: _senhaEC.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cadastro de Usuário'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: _emailnEC,
+                decoration: const InputDecoration(
+                  label: Text('E-mail'),
+                ),
+              ),
+              TextField(
+                controller: _senhaEC,
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: registerUser,
+                  child: const Text('Cadastrar Usuário'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-void main() {
+import 'package:flutter_firebase/auth/email_password/register_page.dart';
+import 'package:flutter_firebase/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primaryColor: Colors.blue,
+        primarySwatch: Colors.blue,
+        floatingActionButtonTheme:
+            const FloatingActionButtonThemeData(backgroundColor: Colors.blue),
+        appBarTheme: const AppBarTheme(
+          color: Colors.blue,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {'/auth/email_password/register': (_) => const RegisterPage()},
+      home: const MyHomePage(
+        title: 'Home Page',
+      ),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed('/auth/email_password/register');
+              },
+              child: const Text("Register User E-mail Password"),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
